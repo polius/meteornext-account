6d94e87682ed22e488a0507a313aa26105024f3a
Pol Alzina <gogs@fake.local>
2021-11-22 20:06:11 +0000

    Removed Cancel Subscription

--- client/src/components/billing/Billing.vue
+++ client/src/components/billing/Billing.vue
@@ -3,20 +3,17 @@
     <Summary :account="account"/>
     <v-divider style="margin-top:20px; margin-bottom:20px"></v-divider>
     <Purchases :account="account"/>
-    <v-divider style="margin-top:20px; margin-bottom:20px"></v-divider>
-    <Cancel :account="account"/>
   </div>
 </template>
 
 <script>
 import Summary from './Summary'
 import Purchases from './Purchases'
-import Cancel from './Cancel'
 
 export default {
   data: () => ({
   }),
-  components: { Summary, Purchases, Cancel },
+  components: { Summary, Purchases },
   props: {
     account: Object
   },
